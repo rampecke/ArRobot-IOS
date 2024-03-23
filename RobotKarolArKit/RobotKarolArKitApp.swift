e2a8e3d12085b325a551e88576cdf5fb12d20116
Ramona_Eckert <ge36moc@mytum.de>
2024-03-22 22:26:51 -0700

    Added XCodeProject

---
+++ RobotKarolArKit/RobotKarolArKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  RobotKarolArKitApp.swift
+//  RobotKarolArKit
+//
+//  Created by Ramona Eckert on 22.03.24.
+//
+
+import SwiftUI
+
+@main
+struct RobotKarolArKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
