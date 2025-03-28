468839d6450e4247fdec3aca6e4aaeb463018885
Ramona_Eckert <ge36moc@mytum.de>
2025-03-28 17:27:15 +0100

    Create a server instance and move into seperate folders

---
+++ Server/roboCraft/src/main/java/com/ramonaeckert/roboCraft/RoboCraftApplication.java
@@ -0,0 +1,13 @@
+package com.ramonaeckert.roboCraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoboCraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoboCraftApplication.class, args);
+	}
+
+}
